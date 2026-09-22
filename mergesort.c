9e473f780442d0f506bb68cc700cf682f65777bb
Wyvernhunter345 <syndix01@outlook.com>
2026-09-22 20:43:55 +0930

    Add basic gitignore and broken segfaulting threading code

--- mergesort.c
+++ mergesort.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <string.h> /* for memcpy */
 #include <stdlib.h> /* for malloc */
+#include <pthread.h>
 #include "mergesort.h"
 
 /* this function will be called by mergesort() and also by parallel_mergesort(). */
@@ -13,15 +14,48 @@ void merge(int leftstart, int leftend, int rightstart, int rightend){
 
 /* this function will be called by parallel_mergesort() as its base case. */
 void my_mergesort(int left, int right){
+	printf("Reached Base Case\n");
+	fflush(stdout);
 }
 
 /* this function will be called by the testing program. */
 void * parallel_mergesort(void *arg){
+	struct argument *variables = (struct argument *) arg;
+
+	// Base case
+	if (variables->level == 0)
+	{
+		my_mergesort(variables->left, variables->right);
 		return NULL;
+	}
+
+	// Recursive case
+	pthread_t p1, p2;
+	printf("We at level %u\n", variables->level);
+	fflush(stdout);
+	variables->level = variables->level - 1;
+	pthread_create(&p1, NULL, parallel_mergesort(variables), "A");
+	pthread_create(&p2, NULL, parallel_mergesort(variables), "B");
+	pthread_join(p1, NULL);
+	pthread_join(p, NULL);
+	return NULL;
 }
 
 /* we build the argument for the parallel_mergesort function. */
 struct argument * buildArgs(int left, int right, int level){
-		return NULL;
+	struct argument
+	{
+		int left;
+		int right;
+		int level;
+	};
+	
+	struct argument *arg = malloc(sizeof(struct argument));
+	arg->left = left;
+	arg->right = right;
+	arg->level = level;
+	
+	// all params will be included in this one struct - this goes to parallel_mergesort
+	return arg;
 }
 
